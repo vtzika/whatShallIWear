9240d0ce916733b3cd361157923401357f9ccef7
Varvara Tzika <varvara@Varvaras-MacBook-Pro.local>
2014-08-11 11:41:36 +0200

    Sources admin added

--- sources/admin.py
+++ sources/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from sources.models import Source
 
-# Register your models here.
+
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ('source_url', 'gender', 'place')
+
+admin.site.register(Source, SourceAdmin)
